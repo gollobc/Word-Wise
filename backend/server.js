0f9b6894266d674fa4da47f9e92f4d5f99e7c27d
gollobc <cadegollob@gmail.com>
2023-08-14 09:54:45 -0700

    deploy test

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,8 @@ app.use(cors())
 // body parser - used for POST/PUT/PATCH routes:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+// use the React build folder for static files
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
 
 /* Mount routes
@@ -33,6 +35,12 @@ app.use(express.json())
 // This tells our app to look at the `controllers/collections.js` file 
 // to handle all routes that begin with `localhost:3000/api/collection`
 app.use('/api/collection', collectionsCtrl)
+
+// Any other route not matching the routes above gets routed by React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
+});
+
 
 /* Tell the app to listen on the specified port
 --------------------------------------------------------------- */
